ce9d22d3ac8c68efc20941c0e458894ad181e1a6
raechelkelso <raechelmkelso@gmail.com>
2021-07-25 15:07:49 -0700

    adjusted photos and contact form

--- clothedbyalliep.js
+++ clothedbyalliep.js
@@ -1,27 +1,16 @@
 
-//submit form//
-function validateForm() {
-  let x = document.forms["myForm"]["fname"].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-}
-
-//photo slide//
 
 
 
 
-function validateForm() {
-  const userEmail =getElementById('userEmail');
-
-  const userEmail =getElementById('userEmail').value;
-
-if (userEmail.includes('@') && userEmail.length > 5) {
-  return true 
-} else {
-  alert ('invaild email')
-  return false
-  }
+function validateForm()
+{
+	var x = document.forms["contactform"]["name"].value;
+	if (x == null || x == "" || x == "Name") {
+		alert("First name must be filled out");
+		return false;
+	}
+	else {
+		return true;
+	}
 }
--- photos.js
+++ photos.js
@@ -1,8 +1,11 @@
-function upDate(element){
+
+
+ //*** CLASS REQUIRMENT OF JAVASCRIPT CHANGING TEXT WHEN HOVERED OVER PHOTOS***  
+function upDate(element) {
     document.getElementById('image').innerHTML = element.alt;
   }
   function unDo(){
-    document.getElementById('image').innerHTML = "peep it.";
+    document.getElementById('image').innerHTML = "Sample Work";
   }
   function openFunction(){
     el = document.getElementById("content");
